ad9b376e4da799e689f115451b740ccf14146a1a
Mansahib Singh <mansahib.singh@mansahibss496.successivetech.com>
2025-06-19 15:15:34 +0530

    update features/assignment-1 add navbar for different assignments

--- assignment-react/my-app/src/app/layout.js
+++ assignment-react/my-app/src/app/layout.js
@@ -20,27 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     <div>
-     <Link href="/assignment-1/ques1">
-        <button>ques1</button>
+      <div>
+      <Link href={"/assignment-1"}>
+      assignment1
       </Link>
-      <Link href="/assignment-1/ques2">
-        <button>ques2</button>
-      </Link>
-      <Link href="/assignment-1/ques3">
-        <button>ques3</button>
-      </Link>
-      <Link href="/assignment-1/ques4">
-        <button>ques4</button>
-      </Link>
-      <Link href="/assignment-1/ques5">
-        <button>ques5</button>
-      </Link>
-      <Link href="/assignment-1/ques6">
-        <button>ques6</button>
-      </Link>
-      
-     </div>
+      </div>
       
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
