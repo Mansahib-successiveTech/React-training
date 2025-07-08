cb2dbcbfb9cc0924db9a94293c9e328ce2c03fa6
Mansahib Singh <mansahib.singh@mansahibvrs16.successivetech.com>
2025-07-08 12:10:21 +0530

    assignment 1 and 2 converted in typeScript

---
+++ assignment-react/my-app/src/components/ChildAuth.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+
+const ChildAuth =(() => {
+  const { loggedIn, login } = useAuth();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState(0);
+  const onClickHandler=()=>{
+    login(name, password)
+   // setName("")
+    setPassword(0);
+  }
+  return (
+    <>
+     <p>{loggedIn ? ` user loged in ${name}` : "please login"}</p>
+      {
+      loggedIn?"welcome":<> <label>username</label>
+      <input
+        value={name}
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label>password</label>
+      <input
+        value={password}
+        type="number"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <div>
+        <button onClick={onClickHandler}>login</button>
+      </div>
+      </>
+      }
+
+     
+     
+    </>
+  );
+});
+export default ChildAuth;
---
+++ assignment-react/my-app/src/components/withAuth.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useAuth } from "../context/AuthContext";
+import { redirect } from "next/navigation";
+
+const withAuth = (WrappedComponent) => (props) => {
+  const {loggedIn}=useAuth();
+  
+
+  if (!loggedIn) {
+    redirect("/login");
+  }
+
+  return <WrappedComponent {...props} />;
+};
+
+export default withAuth;
